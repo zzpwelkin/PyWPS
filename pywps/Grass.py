03901a2485071127cbad09350cb05d8a6129b773
zzpwelkin <zzpwelkin@gmail.com>
2012-11-01 19:16:20 +0800

    not set the LD_LIBRARY_PATH from configure

--- pywps/Grass.py
+++ pywps/Grass.py
@@ -49,7 +49,7 @@ class Grass:
                 "version":"GRASS_VERSION",
                 "gui":"GRASS_GUI",
                 "gisbase": "GISBASE",
-                "ldLibraryPath": "LD_LIBRARY_PATH"
+                #"ldLibraryPath": "LD_LIBRARY_PATH"
         }
 
         # put env
